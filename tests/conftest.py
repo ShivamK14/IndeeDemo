a96eb6dd950e0957b1be80fca3c7dbd9c55b004b
ShivamK14 <shivam.karle@gmail.com>
2024-11-28 13:13:11 +0530

    third commit

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,17 +1,16 @@
 import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
-
 from BaseClass import BaseClass
 from PageObject import VideoPlayer
 
 
 
-
+# service=Service('C:\\Users\\Lenovo\\Desktop\\indee\\IndeeDemo\\chromedriver.exe')
 #we are setting up Fixture for setup  , here our webdriver is getting initialise
 @pytest.fixture(scope='class')
 def setup(request):
-    driver = webdriver.Chrome(service=Service('C:\\Users\\shivakar\\OneDrive - AMDOCS\\Backup Folders\\Desktop\\Indee\\chromedriver.exe'))
+    driver = webdriver.Chrome()
     driver.get(BaseClass.url)
     driver.maximize_window()
     request.cls.driver=driver
